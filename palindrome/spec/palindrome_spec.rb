ef1c2a40d71def144e112ee00c6c2a606559c404
Andrew Cox <andrew@andrewcox.org>
2023-03-26 08:35:16 -0400

    Reset palindrome kata

--- palindrome/spec/palindrome_spec.rb
+++ palindrome/spec/palindrome_spec.rb
@@ -4,24 +4,24 @@ require "rspec"
 require "palindrome"
 
 describe Palindrome do
-  describe ".check" do
+  describe ".valid?" do
     it "returns true for palindromes" do
-      expect(Palindrome.check("A man, a plan, a canal: Panama")).to be true
-      expect(Palindrome.check("Was it a car or a cat I saw?")).to be true
-      expect(Palindrome.check("Madam, in Eden, I'm Adam.")).to be true
-      expect(Palindrome.check("racecar")).to be true
+      expect(Palindrome.valid?("A man, a plan, a canal: Panama")).to be true
+      expect(Palindrome.valid?("Was it a car or a cat I saw?")).to be true
+      expect(Palindrome.valid?("Madam, in Eden, I'm Adam.")).to be true
+      expect(Palindrome.valid?("racecar")).to be true
     end
 
     it "returns false for non-palindromes" do
-      expect(Palindrome.check("hello")).to be false
-      expect(Palindrome.check("world")).to be false
-      expect(Palindrome.check("ruby")).to be false
+      expect(Palindrome.valid?("hello")).to be false
+      expect(Palindrome.valid?("world")).to be false
+      expect(Palindrome.valid?("ruby")).to be false
     end
 
     it "ignores non-alphanumeric characters" do
-      expect(Palindrome.check("A man, a plan, a canal -- Panama")).to be true
-      expect(Palindrome.check("Mr. Owl ate my metal worm")).to be true
-      expect(Palindrome.check("Was it a car or a cat I saw?")).to be true
+      expect(Palindrome.valid?("A man, a plan, a canal -- Panama")).to be true
+      expect(Palindrome.valid?("Mr. Owl ate my metal worm")).to be true
+      expect(Palindrome.valid?("Was it a car or a cat I saw?")).to be true
     end
   end
 end
